882d6b78b06abc144f2700112270a21f30a41afd
DarkAngelGR <asavvo01@gmail.com>
2019-04-04 21:29:37 +0000

    LOS-ify

--- device.mk
+++ device.mk
@@ -22,6 +22,7 @@ $(call inherit-product, vendor/xiaomi/markw/markw-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay-lineage
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
