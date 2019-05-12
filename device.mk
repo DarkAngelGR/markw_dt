bdca7843d10e2af85b37fe86dbc305faadaa141b
rpeter85 <deus@blackpanther.hu>
2019-05-12 16:18:00 +0200

    Changed some Dalvik heap setting

--- device.mk
+++ device.mk
@@ -76,11 +76,11 @@ PRODUCT_COPY_FILES += \
 
 # HWUI
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapgrowthlimit=288m \
-    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapstartsize=16m \
     dalvik.vm.heapsize=768m \
     dalvik.vm.heaptargetutilization=0.75 \
-    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapminfree=4m \
     dalvik.vm.heapmaxfree=8m
 
 PRODUCT_PROPERTY_OVERRIDES += \
