2bee14f3764020b40e70e11b895a5dc8dec5643a
jmjxperia <jmjxperia@users.noreply.github.com>
2019-05-12 21:21:01 +0530

    YUREKA2: partial rebranding into YU Settings

--- device.mk
+++ device.mk
@@ -183,9 +183,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_mido
 
-# Xiaomi Shit
+# YUSettings
 PRODUCT_PACKAGES += \
-    XiaomiShit
+    YUSettings
 
 # FM
 PRODUCT_PACKAGES += \
